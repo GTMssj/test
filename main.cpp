843b6bb9cac40e44577db3c41a7a1f876d79bd16
Debian <SteveTao@DebianPers.tail1853f.ts.net>
2024-06-19 08:00:00 +0000

    need input cin >>

--- main.cpp
+++ main.cpp
@@ -5,17 +5,42 @@ using namespace std;
 
 class Bot{
 public:
-	int posx = 3;
-	int posy = 1;
+	int posx = 0;
+	int posy = 0;
 
-	Bot(){
+	Bot(int n, int m){
+		maxx = m;
+		maxy = n;
 	}
 
+	void move(char dir, int step, char* arr){
+		arr[maxx*posy + posx] = '+';
+		switch (dir){
+			case 'U':
+				posy -= step;
+				for(int i = 0; i < step; i ++) arr[maxx*(posy-i) + posx] = '+';
+				break;
+			case 'D':
+				for(int i = 0; i < step; i ++) arr[maxx*(posy+i) + posx] = '+';
+				posy += step;
+				break;
+			case 'L':
+				for(int i = 0; i < step; i ++) arr[maxx*posy + posx - i] = '+';
+				posx -= step;
+				break;
+			case 'R':
+				for(int i = 0; i < step; i ++) arr[maxx*posy + posx + i] = '+';
+				posx += step;
+				break;
+		}
+		//cout << dir << ", " << posx << ", " << posy << endl;
+	}
 private:
+	int maxx, maxy;
 };
 
-int probe(char dir, int* arr, int n, int m, int x, int y){
-	if (arr[m*y + x] != 0) return 0;
+int probe(char dir, char* arr, int n, int m, int x, int y){
+	if (arr[m*y + x] == '#') return 0;
 	int i = 0;
 	int j = 0; 
 	int step = 0;
@@ -35,7 +60,7 @@ int probe(char dir, int* arr, int n, int m, int x, int y){
 			break;
 	}
 	
-	while(arr[m*(y+i) + x + j] == 0 && x+j >= 0 && x+j < m && y+i >= 0 && y+i < n){
+	while(arr[m*(y+i) + x + j] != '#' && x+j >= 0 && x+j < m && y+i >= 0 && y+i < n){
 		step ++;
 		x += j;
 		y += i;
@@ -44,35 +69,67 @@ int probe(char dir, int* arr, int n, int m, int x, int y){
 	return step;
 }
 
-string cmd2mov(string inp){
-	stack<int> st;
-}
-
 int main(){
 	int n = 3;
 	int m = 7;
-	int matrix[n][m] = {
-		{0,0,0,1,0,0,0}, 
-		{0,0,0,1,0,0,0}, 
-		{0,0,0,0,0,0,0}
+	char matrix[n][m] = {
+		{'.','.','.','#','.','.','.'}, 
+		{'#','#','.','.','.','.','.'}, 
+		{'.','.','.','.','.','.','.'}
 	};
-	for(int i = 0; i < 3; i ++){
-		for(int j = 0; j < 7; j ++){
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < m; j ++){
+			cout << matrix[i][j];
+		}
+		cout << endl;
+	}
+
+	Bot bot(n, m);
+	//string cmd = "UDD(RRD(R)*D)3";
+	string cmd = "(RDRUL)5";
+	cout << cmd << endl;
+
+	stack<int> s;
+	int lind, rind, iteration;
+
+	for(int i = 0; i < cmd.length(); i ++){
+		if(cmd[i] == '(') s.push(i);
+		if(cmd[i] == ')' && cmd[i+1] != '*'){
+			lind = s.top();
+			s.pop();
+			rind = i;
+			string itsub = cmd.substr(rind+1, 1);
+			int iteration = stoi(itsub);
+			string sub = cmd.substr(lind+1, rind-lind-1);
+			cout << sub << endl;
+			string rep;
+			for(int j = 0; j < iteration; j ++){
+				rep += sub;
+			}
+			cmd.replace(lind, rind-lind+2, rep);
+			cout << cmd << endl;
+		}
+	}
+	while (cmd.length() > 0){
+		int num = 1;
+		if (cmd[0] != '('){
+			while (cmd[num] == cmd[0]){
+				num ++;
+			}
+			int step_probe = probe(cmd[0], &matrix[0][0], n, m, bot.posx, bot.posy);
+			bot.move(cmd[0], step_probe > num ? num : step_probe, &matrix[0][0]);
+			cmd.erase(0, num);
+		}
+		else{
+			bot.move(cmd[1], probe(cmd[1], &matrix[0][0], n, m, bot.posx, bot.posy), &matrix[0][0]);
+			cmd.erase(0, 4);
+		}
+	}
+	for(int i = 0; i < n; i ++){
+		for(int j = 0; j < m; j ++){
 			cout << matrix[i][j];
 		}
 		cout << endl;
 	}
 	cout << endl;
-
-	Bot bot;
-	/*
-	cout << "U:" << probe('U', &matrix[0][0], n, m, bot.posx, bot.posy);
-	cout << ", D:" << probe('D', &matrix[0][0], n, m, bot.posx, bot.posy);
-	cout << ", L:" << probe('L', &matrix[0][0], n, m, bot.posx, bot.posy);
-	cout << ", R:" << probe('R', &matrix[0][0], n, m, bot.posx, bot.posy);
-	cout << endl;
-	*/
-	string cmd;
-	cin >> cmd;
-	cout << cmd << endl;
 }
